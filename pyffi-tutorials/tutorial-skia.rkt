0e979bbd460b66fcb5f1ddaf03329c5b8f1020b5
Jens Axel Søgaard <jensaxel@soegaard.net>
2022-08-05 00:02:02 +0200

    Add heptagram to Skia examples

--- pyffi-tutorials/tutorial-skia.rkt
+++ pyffi-tutorials/tutorial-skia.rkt
@@ -20,48 +20,85 @@
 (import skia)
 (define None (void))
 
-;;; Circle
 
-(define (circle)
-  (define width  200)
-  (define height 200)
-  
+;;;
+;;; DRAW
+;;;
+
+; All examples draw on a canvas and saves the result in a png file.
+
+(define (draw f filename [width 300] [height 300])
   (define surface (skia.Surface width height))
-  
+  (define canvas  (surface.__enter__))
+  (f canvas)
+  (surface.__exit__ None None None)
+  (define image   (surface.makeImageSnapshot))
+  (image.save filename skia.kPNG))
 
-  ; (with surface as canvas : ...)
-  (define canvas (surface.__enter__))
-  (canvas.drawCircle 100 100 40 (skia.Paint #:Color skia.ColorRED))
-  (surface.__exit__ None None None) ; None means no exceptions occured
 
-  (define image (surface.makeImageSnapshot))
-  (image.save "output-circle.png" skia.kPNG))
+;;;
+;;; Circle
+;;;
 
+(define (circle canvas)
+  (canvas.drawCircle 100 100 40 (skia.Paint #:Color skia.ColorRED)))
+
+;;;
 ;;; Drop Shadow
+;;;
 
 ;; canvas.drawColor(skia.ColorWHITE)
 ;; paint = skia.Paint(ImageFilter=skia.ImageFilters.DropShadow(3, 3, 5, 5, skia.ColorBLACK))
 ;; blob = skia.TextBlob("Skia", skia.Font(None, 120))
 ;; canvas.drawTextBlob(blob, 0, 160, paint)
 
-(define (dropshadow)
-  (define width  200)
-  (define height 200)
-  (define surface (skia.Surface width height))
-
-  (define canvas (surface.__enter__))
-
+(define (dropshadow canvas)
   (canvas.drawColor skia.ColorWHITE)
   (define paint (skia.Paint #:ImageFilter (skia.ImageFilters.DropShadow 3 3 5 5 skia.ColorBLACK)))
   (define blob  (skia.TextBlob "Skia" (skia.Font None 120)))
-  (canvas.drawTextBlob blob 0 160 paint)
+  (canvas.drawTextBlob blob 0 160 paint))
 
-  (surface.__exit__ None None None)
+;;;
+;;; Heptagram
+;;;
+
+;; void draw(SkCanvas* canvas) {
+;;     const SkScalar scale = 256.0f;
+;;     const SkScalar R = 0.45f * scale;
+;;     const SkScalar TAU = 6.2831853f;
+;;     SkPath path;
+;;     path.moveTo(R, 0.0f);
+;;     for (int i = 1; i < 7; ++i) {
+;;         SkScalar theta = 3 * i * TAU / 7;
+;;         path.lineTo(R * cos(theta), R * sin(theta));
+;;     }
+;;     path.close();
+;;     SkPaint p;
+;;     p.setAntiAlias(true);
+;;     canvas->clear(SK_ColorWHITE);
+;;     canvas->translate(0.5f * scale, 0.5f * scale);
+;;     canvas->drawPath(path, p);
+;; }
+
+(define (heptagram canvas)
+  (define scale 256.)
+  (define R     (* 0.45 scale))
+  (define τ     6.2831853)
+
+  (define path (skia.Path))
+  (path .moveTo R 0.)
+  (for ([i (in-range 1 7)])
+    (define θ (* 3/7 i τ))
+    (path .lineTo (* R (cos θ)) (* R (sin θ))))
+  (path .close)
   
-  (define image (surface.makeImageSnapshot))
-  (image.save "output-dropshadow.png" skia.kPNG))
+  (define p (skia.Paint))
+  (p .setAntiAlias #t)
+  (canvas .drawColor skia.ColorWHITE)
+  (canvas .translate (* 0.5 scale) (* 0.5 scale))
+  (canvas .drawPath path p))
 
 
-(circle)
-(dropshadow)
-
+(draw circle      "output-circle.png")
+(draw dropshadow  "output-dropshadow.png")
+(draw heptagram   "output-heptagram.png")
