9f13a3e4dd822d0e2fee30aa3bbb89e36f107c08
Jens Axel Søgaard <jensaxel@soegaard.net>
2022-09-28 22:43:21 +0200

    Add build-deps

--- pyffi-lib/pyffi/info.rkt
+++ pyffi-lib/pyffi/info.rkt
@@ -1,6 +1,7 @@
 #lang info
 
-(define deps (list "base" "at-exp-lib"))
+(define deps       (list "base" "at-exp-lib"))
+(define build-deps (list "base" "at-exp-lib"))
 
 (define raco-commands
   (list (list "pyffi" 'pyffi/configure-pyffi "configure pyffi" #f)))
