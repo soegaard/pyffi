075e9216bf9b72683f2154b39d852b64029e7f61
Jens Axel Søgaard <jensaxel@soegaard.net>
2022-09-29 13:58:49 +0200

    Don't use full path, when full path can't be determined

--- pyffi-lib/pyffi/libpython.rkt
+++ pyffi-lib/pyffi/libpython.rkt
@@ -42,7 +42,7 @@
                   #:when (file-exists? (build-full-path name)))
         (build-full-path name))
       ;; Github Action (Ubuntu)
-      (build-full-path "libpython3.10")))
+      "libpython3.10")) 
 
 
 
