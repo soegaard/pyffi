07010870899e277127f783bd9862e13a14b344c2
Jens Axel Søgaard <jensaxel@soegaard.net>
2025-12-04 13:24:24 +0100

    Add current-error-on-not-found

--- pyffi-lib/pyffi/parameters.rkt
+++ pyffi-lib/pyffi/parameters.rkt
@@ -1,10 +1,13 @@
 #lang racket/base
 (provide current-repr
          current-str
+         current-error-on-not-found?
          current-pygenerator-prop:sequence)
 
 (define current-repr (make-parameter (λ (x) "current-repr not set to `repr` yet")))
 (define current-str  (make-parameter (λ (x) "current-str not set to `str` yet")))
+
+(define current-error-on-not-found? (make-parameter #t))
 
 (define current-pygenerator-prop:sequence
   ; used for prop:sequence of generator-obj structures, see "python-generator.rkt"
