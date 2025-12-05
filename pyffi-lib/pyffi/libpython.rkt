ca49d58778f79a6786b6fd882e0f5db79c489a0f
Jens Axel Søgaard <jensaxel@soegaard.net>
2025-12-05 23:37:31 +0100

    Update version of Github Actions

--- pyffi-lib/pyffi/libpython.rkt
+++ pyffi-lib/pyffi/libpython.rkt
@@ -8,7 +8,7 @@
 
 ;;; Imports
 
-(require ffi/unsafe ffi/unsafe/define racket/file) 
+(require ffi/unsafe ffi/unsafe/define racket/file racket/list) 
 
 
 ;;; Configuration
@@ -23,13 +23,36 @@
     (displayln "See details in the documentation.")
     (exit 1)))
 
-
 (define extension
   (case (system-type 'os)
     [(macosx)  "dylib"]
     [(unix)    "so"]
     [(windows) "dll"]
     [else      (error 'internal-error:extension "File a bug report on Github.")]))
+
+
+;; find-libpython3 : (or/c path-string? #f) -> (or/c path? #f)
+;;   If libpython-folder is a directory, return the path to a file named
+;;     libpython3.xx.<extension>
+;;   inside that directory, or #f if none is found or folder is #f.
+(define (find-libpython3 libpython-folder)
+  (cond
+    [(not libpython-folder) #f]
+    [else
+     (define dir (path->complete-path libpython-folder))
+     (define rx
+       ;; Example on macOS: ^libpython3\.[0-9][0-9]\.dylib$
+       (regexp (format "^libpython3\\.[0-9][0-9]~a$"
+                       (regexp-quote
+                        (string-append "." extension)))))
+     (with-handlers ([exn:fail? (λ (_e) #f)])
+       (for/or ([p (in-list (directory-list dir))])
+         (let-values ([(base name must-dir?) (split-path p)])
+           (and name
+                (let ([s (path->string name)])
+                  (and (regexp-match? rx s)
+                       (build-path libpython-folder p)))))))]))
+
 
 (define (build-full-path name)
   (if libpython-folder
@@ -38,15 +61,16 @@
       (string->path (string-append name "." extension))))
 
 (define libpython-path
-  (or (for/first ([name '("libpython3.10" "libpython310" "libpython3")]
+  (or (find-libpython3 libpython-folder) ; An absolute, full path
+      (for/first ([name '("libpython3" "libpython3.10" "libpython310"
+                                       )]
                   #:when (file-exists? (build-full-path name)))
         (build-full-path name))
       ;; Github Action (Ubuntu)
-      "libpython3.10")) 
+      ;;   On Github Action the `raco pyffi configure` is run after
+      ;;   the documentation is rendered, so we need to provide the version here.
+      "libpython3.14"))  
 
-
-
-; (displayln (list 'libpython-path libpython-path))
 
 ; Note: If the Python interpreter loads a shared library dynamically,
 ;       it needs access to the Python C-API. To make the symbols
@@ -58,3 +82,4 @@
 (define lib (ffi-lib libpython-path #:global? #t))
 
 (define-ffi-definer define-python lib #:default-make-fail make-not-available)
+
